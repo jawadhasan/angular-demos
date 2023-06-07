82c1e72d4818d4af7d612b1e8d0c97023c7465f6
Jawad Shani <jawad.shani@hitachi-eu.com>
2023-06-07 11:49:07 +0200

    update samples component

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,42 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-sample',
-    template: `<div>
-    <h1> {{title | uppercase }} </h1>
-  <p>{{ totalPrice() | currency:'EUR': true }} from method.</p>
+  selector: 'app-sample',
+  template: `<div>
+    <h1>{{ title | uppercase }}</h1>
+    <p>{{ totalPrice() | currency : 'EUR' : true }} from method.</p>
 
-   <input type="text" (keydown)="showKey($event)"/>
-   
-  <button class="btn btn-info" (click)="upQuantity()">Update Qty</button>
-  
-  </div>`
+    <input type="text" (keydown)="showKey($event)" />
+
+    <button class="btn btn-info" (click)="upQuantity()">Update Qty</button>
+  </div>`,
 })
 export class SampleComponent {
-    title = 'Sample component';
-    data = [
-        { id: 1, name: "item-1", price: 10 },
-        { id: 2, name: "item-2", price: 20 },
-        { id: 3, name: "item-3", price: 30 }
-    ];
-    totalPrice() {
-        let sum = 0;
+  title = 'Sample component';
+  data = [
+    { id: 1, name: 'item-1', price: 10 },
+    { id: 2, name: 'item-2', price: 20 },
+    { id: 3, name: 'item-3', price: 30 },
+  ];
+  totalPrice() {
+    let sum = 0;
 
-        for (let item of this.data) {
-            sum += item.price;
-        }
-        return sum;
+    for (let item of this.data) {
+      sum += item.price;
     }
+    return sum;
+  }
 
-    upQuantity(){
-        alert('you clicked upQuantity button')
-    }
+  upQuantity() {
+    alert('you clicked upQuantity button');
+  }
 
-    showKey(event:any){
-        alert(event.keyCode)
-    }
+  showKey(event: any) {
+    console.log(event.keyCode);
+  }
 
-    getCoords(event:any){
-        console.log(event.clientX + ' : ' + event.clientY);
-    }
+  getCoords(event: any) {
+    console.log(event.clientX + ' : ' + event.clientY);
+  }
 }
