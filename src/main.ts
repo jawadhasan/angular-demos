fbd5ecdacfb17d17e337b03fcba92d5b6d78ab06
Jawad Shani <jawad.shani@hitachi-eu.com>
2023-08-14 11:03:03 +0200

    web-vitals package added

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { onLCP, onFID, onCLS } from 'web-vitals';
+import reportWebVitals from './reportWebVitals';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  // Log performance metrics to the console:
+//reportWebVitals(console.log);
+onCLS(console.log);
+onFID(console.log);
+onLCP(console.log);
