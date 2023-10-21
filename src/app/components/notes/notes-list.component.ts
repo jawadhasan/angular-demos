46c9412c4cc07f647124a37ec6e2a174f098f151
Jawad Shani <jawad.shani@hitachi-eu.com>
2023-10-21 22:57:19 +0200

    rxjs for notes

--- src/app/components/notes/notes-list.component.ts
+++ src/app/components/notes/notes-list.component.ts
@@ -1,41 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
+import { BehaviorSubject, EMPTY, catchError, combineLatest, map } from 'rxjs';
+import { NotesService } from 'src/app/components/notes/notes.service';
 
 @Component({
   selector: 'app-notes-list',
-  template: `<div>
-    <h3>NOTES</h3>
-
-    <!-- Select -->
-    <div class="row">
-      <div class="col col-md-4">
-        <select
-          class="form-select"
-          [(ngModel)]="selectedCategory"
-          (change)="categoryChanged(selectedCategory)"
-        >
-          <option *ngFor="let category of categories" [ngValue]="category">
-            {{ category.name }}
-          </option>
-        </select>
-      </div>
-    </div>
-
-    <hr />
-
-    <div class="row">
-      <!-- Cards List -->
-      <div *ngFor="let note of notes" class="col-4 my-3">
-        <app-notes-card [note]="note"></app-notes-card>
-      </div>
-    </div>
-    <p class="float-right"><b>Jawad Hasan Shani</b></p>
-  </div>`,
+  templateUrl: './notes-list.component.html',
   styles: [''],
 })
 export class NotesListComponent implements OnInit {
-  notes: any[];
-  orgNotes: [];
+  title: string = 'NOTES';
+
+  // notes$ = this.notesService.notes$.pipe(
+  //     catchError(err=> EMPTY)
+  //   );
+
+  notes$ = combineLatest([
+    this.notesService.notes$,
+    this.notesService.selectedCategoryAction$,
+  ]).pipe(
+    map(([notes, categoryFilter]) =>
+      //JavaScript array filter
+      notes.filter((note) =>
+        categoryFilter.id > 0 ? note.categoryId === categoryFilter.id : true
+      )
+    ),
+    catchError((err) => EMPTY)
+  );
+
   selectedCategory: any;
   categories: any[] = [
     { id: 0, name: 'ALL' },
@@ -48,37 +39,15 @@ export class NotesListComponent implements OnInit {
 
     { id: 100, name: 'General' },
   ];
-  constructor(private apiService: ApiService) {}
+
+  constructor(private notesService: NotesService) {}
 
   ngOnInit(): void {
-    //http AJAX call
-    this.apiService.getNotes().subscribe(
-      (data: any) => {
-        console.log(data);
-
-
-
-
-        this.notes = data;
-        this.orgNotes = data; //saving copy of the original data
-
-        this.selectedCategory = this.categories[0]; //todo //this.categories.find(cat => cat.id === 20); //first category as default
-      },
-      (err: any) => console.log(err)
-    ); //this.notes = data.result);
+    this.selectedCategory = this.categories[0];
   }
 
   //category select
   categoryChanged(e): void {
-    console.log(e);
-    this.notes = this.orgNotes.slice(); //refreshing local-data
-
-    //TODO: from API
-    if (e.id == 0) {
-      //ALL
-    } else {
-      //apply filter
-      this.notes = this.notes.filter((note) => note.categoryId == e.id); //return this.notes.find(note => note.categoryId === e.id)
-    }
+    this.notesService.selectCategory(e);
   }
 }
---
+++ src/app/components/notes/notes.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotesService {
+  private notesUrl: string =
+    'https://kjsa0fp0tb.execute-api.eu-central-1.amazonaws.com/Prod/api/notes';
+
+  notes$ = this.httpClient
+    .get(`${this.notesUrl}`)
+    .pipe(catchError(this.handleError));
+
+  //create an action-stream
+  private selectedCategorySubject = new BehaviorSubject<any>({});
+  selectedCategoryAction$ = this.selectedCategorySubject.asObservable();
+
+  constructor(private httpClient: HttpClient) {}
+
+  selectCategory(category: any) {
+    console.log(category);
+    this.selectedCategorySubject.next(category);
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<any> {
+    console.error('server error:', error);
+
+    if (error.error instanceof Error) {
+      const errMessage = error.error.message;
+      //return Observable.throw(errMessage);
+      return throwError(() => errMessage);
+    }
+    // return Observable.throw(error || 'ASP.NET Core server error');
+
+    return throwError(
+      () => new Error(error?.message || 'ASP.NET Core server error')
+    );
+  }
+}
