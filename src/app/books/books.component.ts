cd7ffdbf6964d7e0e84f9fdb0bedc840f1a722af
Jawad Shani <jawad.shani@hitachi-eu.com>
2024-09-21 14:04:03 +0200

    book detail added

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -9,14 +9,22 @@ import { BooksService } from './books.service';
 export class BooksComponent {
   msg:string='';
   selectedCategoryId:number=0;
+  selectedBookId:number=0;
 
   booksCategories$ = this.booksService.booksCategories$;
   books$ = this.booksService.books$;
+  selectedBook$ = this.booksService.selectedBook$;
 
   constructor(private booksService:BooksService) { }
 
   selectCategory(category:any){
     this.booksService.selectCategory(category.id);
     this.selectedCategoryId=category.id;
+  }
+
+  selectBook(book:any){
+    this.booksService.selectBook(book.id);
+    this.selectedBookId=book.id;
+
   }
 }
--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {  HttpClient,  HttpErrorResponse} from '@angular/common/http';
-import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable, Subject, throwError } from 'rxjs';
 import { map, catchError, switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
@@ -8,20 +8,29 @@ import { map, catchError, switchMap, tap } from 'rxjs/operators';
 })
 export class BooksService {
 
-  private booksApi: string =
+  private booksApi2: string =
     'https://kjsa0fp0tb.execute-api.eu-central-1.amazonaws.com/Prod/api/books';
 
-    //create an action-stream
+
+    private booksApi: string = 'https://localhost:44379/api/books';
+
+
+    //create an action-stream for category selection
     private selectedCategorySubject = new BehaviorSubject<any>(100);
     selectedCategoryAction$ = this.selectedCategorySubject.asObservable();
 
-  // books$ = this.httpClient
-  //   .get(`${this.booksApi}`)
-  //   .pipe(catchError(this.handleError));
+    //create an action-stream for book selection
+    private selectedBookSub = new Subject<number>();
+    selectedBookAction$ = this.selectedBookSub.asObservable();
+
 
   booksCategories$ = this.httpClient
     .get(`${this.booksApi}/categories`)
     .pipe(catchError(this.handleError));
+
+  // books$ = this.httpClient
+  //   .get(`${this.booksApi}`)
+  //   .pipe(catchError(this.handleError));
 
 
 books$ = this.selectedCategoryAction$.pipe(
@@ -36,9 +45,33 @@ books$ = this.selectedCategoryAction$.pipe(
 
 
 
+//have one detail and load-book approach
+selectedBook1$ = this.selectedBookAction$.pipe(
+
+  switchMap(id=> this.httpClient.get(`${this.booksApi}/${id}`)
+  .pipe(
+       catchError(this.handleError)
+  ))
+
+);
 
 
+//Existing data Approach
+selectedBook$ = combineLatest([
+  this.books$,
+  this.selectedBookAction$,
+]).pipe(
+  map(([books, booksFilterId]) =>
 
+    //JavaScript array filter
+    // books.filter((book) =>
+    //   booksFilterId > 0 ? book.categoryId === booksFilterId : true
+    // )
+    books.find(b=> b.id === booksFilterId)
+
+  ),
+  catchError(this.handleError)
+);
 
   constructor(private httpClient: HttpClient) {}
 
@@ -46,6 +79,11 @@ books$ = this.selectedCategoryAction$.pipe(
   selectCategory(category: number) {
     this.selectedCategorySubject.next(category);
   }
+
+    //emit value into action-stream
+    selectBook(bookId: any) {
+      this.selectedBookSub.next(bookId);
+    }
 
   private handleError(error: HttpErrorResponse): Observable<any> {
     console.error('server error:', error);
